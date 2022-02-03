fd2808b03ecf4029857771015c0d96b9edef2b25
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-02-03 15:11:34 +0900

    fix: Link class field

--- controller/router_test.py
+++ controller/router_test.py
@@ -10,7 +10,7 @@ class RouterTest(unittest.TestCase):
         """
         src = Node("n1")
         dst = Node("n2")
-        links = [Link("l1", src.name, dst.name, 1)]
+        links = [Link(src.name, dst.name, 1)]
 
         router = Router([src, dst], links, src, dst)
         path = router.calc_shortest_path()
@@ -35,39 +35,39 @@ class RouterTest(unittest.TestCase):
         """
         nodes = [Node(f"n{i}") for i in range(1, 17)]
         links = [
-            Link("l1", "n1", "n2", 2),
-            Link("l2", "n1", "n5", 3),
-            Link("l3", "n2", "n3", 3),
-            Link("l4", "n2", "n5", 1),
-            Link("l5", "n2", "n6", 1),
-            Link("l6", "n3", "n4", 1),
-            Link("l7", "n3", "n6", 2),
-            Link("l8", "n3", "n7", 2),
-            Link("l9", "n4", "n7", 3),
-            Link("l10", "n4", "n8", 2),
-            Link("l11", "n5", "n6", 5),
-            Link("l12", "n5", "n9", 2),
-            Link("l13", "n6", "n7", 3),
-            Link("l14", "n6", "n9", 2),
-            Link("l15", "n6", "n10", 1),
-            Link("l16", "n6", "n11", 1),
-            Link("l17", "n7", "n8", 2),
-            Link("l18", "n7", "n11", 2),
-            Link("l19", "n8", "n11", 5),
-            Link("l20", "n8", "n12", 1),
-            Link("l21", "n9", "n10", 6),
-            Link("l22", "n9", "n13", 1),
-            Link("l23", "n9", "n14", 1),
-            Link("l24", "n10", "n11", 1),
-            Link("l25", "n10", "n14", 3),
-            Link("l26", "n11", "n12", 1),
-            Link("l27", "n11", "n14", 1),
-            Link("l28", "n11", "n15", 2),
-            Link("l29", "n12", "n15", 5),
-            Link("l30", "n12", "n16", 2),
-            Link("l31", "n13", "n14", 3),
-            Link("l32", "n14", "n15", 2),
-            Link("l33", "n15", "n16", 4),
+            Link("n1", "n2", 2),
+            Link("n1", "n5", 3),
+            Link("n2", "n3", 3),
+            Link("n2", "n5", 1),
+            Link("n2", "n6", 1),
+            Link("n3", "n4", 1),
+            Link("n3", "n6", 2),
+            Link("n3", "n7", 2),
+            Link("n4", "n7", 3),
+            Link("n4", "n8", 2),
+            Link("n5", "n6", 5),
+            Link("n5", "n9", 2),
+            Link("n6", "n7", 3),
+            Link("n6", "n9", 2),
+            Link("n6", "n10", 1),
+            Link("n6", "n11", 1),
+            Link("n7", "n8", 2),
+            Link("n7", "n11", 2),
+            Link("n8", "n11", 5),
+            Link("n8", "n12", 1),
+            Link("n9", "n10", 6),
+            Link("n9", "n13", 1),
+            Link("n9", "n14", 1),
+            Link("n10", "n11", 1),
+            Link("n10", "n14", 3),
+            Link("n11", "n12", 1),
+            Link("n11", "n14", 1),
+            Link("n11", "n15", 2),
+            Link("n12", "n15", 5),
+            Link("n12", "n16", 2),
+            Link("n13", "n14", 3),
+            Link("n14", "n15", 2),
+            Link("n15", "n16", 4),
         ]
 
         router = Router(nodes, links, Node("n13"), Node("n4"))
