57acfd53f07535e7795c714626fceb175ccbab2c
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-02-09 09:41:59 +0900

    fix: add LF

--- topology/main.py
+++ topology/main.py
@@ -45,7 +45,7 @@ def setup(receiver, switches):
     for s in switches:
         s.vsctl('set bridge', s, 'stp-enable=true')
 
-    receiver.cmd(f"./bin/server -v > server/server.log &")
+    receiver.cmd(f"./bin/server -v > server/server.log 2>&1 &")
     # receiver.cmd(f"./bin/server -v > server/{datetime.now().strftime('%Y%m%d_%H%M%S')}.log &")
 
     info('*** waiting to set STP...\n')
@@ -53,8 +53,8 @@ def setup(receiver, switches):
 
 
 def run_disaster(receiver, sender, switches):
-    info("*** Disaster was predicted and start emergency backup")
-    sender.cmd(f"./bin/client -addr {receiver.IP()}:44300 -file 1G.txt > client/client.log &")
+    info("*** Disaster was predicted and start emergency backup!\n")
+    sender.cmd(f"./bin/client -addr {receiver.IP()}:44300 -chunk 1G.txt > client/client.log 2>&1 &")
 
     # time until disaster arrives
     sleep(10)
