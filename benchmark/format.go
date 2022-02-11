60b77f594272df2e96d9684efabc94536555f332
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-02-11 20:38:42 +0900

    fix: unit of calculation for byte size

--- benchmark/format.go
+++ benchmark/format.go
@@ -2,21 +2,20 @@ package benchmark
 
 import (
 	"fmt"
-	"math"
 )
 
 func FormatSize(size int64) string {
-	k := int64(math.Pow(2, 10))
+	var k int64 = 1e3
 	if size < k {
 		return fmt.Sprintf("%dB", size)
 	}
 
-	m := int64(math.Pow(2, 20))
+	var m int64 = 1e6
 	if size < m {
 		return fmt.Sprintf("%dKB", size/k)
 	}
 
-	g := int64(math.Pow(2, 30))
+	var g int64 = 1e9
 	if size < g {
 		return fmt.Sprintf("%dMB", size/m)
 	}
