2da92444638885f4368134d70d77e89396751903
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-02-22 17:40:12 +0900

    fix: change column name to make unit of data size to Byte from GByte

--- db/init.sql
+++ db/init.sql
@@ -16,20 +16,20 @@ CREATE TABLE IF NOT EXISTS experiments
 
 CREATE TABLE IF NOT EXISTS backup_pairs
 (
-    experiment_id INT,
-    name          VARCHAR(255),
-    data_size_gb  BIGINT,
-    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    experiment_id  INT,
+    name           VARCHAR(255),
+    data_size_byte BIGINT,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (experiment_id, name),
     FOREIGN KEY (experiment_id) REFERENCES experiments (id)
 ) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS benchmarks
 (
-    experiment_id         INT,
-    backup_pair_name      VARCHAR(255),
-    received_data_size_gb BIGINT,
-    created_at            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    experiment_id           INT,
+    backup_pair_name        VARCHAR(255),
+    received_data_size_byte BIGINT,
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (experiment_id, backup_pair_name),
     FOREIGN KEY (experiment_id, backup_pair_name) REFERENCES backup_pairs (experiment_id, name)
 ) ENGINE = INNODB;
