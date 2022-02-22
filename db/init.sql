d3765582fe69a3b597a2a7826cc09d05c9b04afb
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-02-22 17:47:57 +0900

    feat: add not null restriction

--- db/init.sql
+++ db/init.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS networks
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    name       VARCHAR(255)                                                    NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP                             NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 ) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS experiments
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
-    network_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    network_id INT                                 NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (network_id) REFERENCES networks (id)
 ) ENGINE = INNODB;
 
@@ -18,8 +18,8 @@ CREATE TABLE IF NOT EXISTS backup_pairs
 (
     experiment_id  INT,
     name           VARCHAR(255),
-    data_size_byte BIGINT,
-    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_size_byte BIGINT                              NOT NULL,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (experiment_id, name),
     FOREIGN KEY (experiment_id) REFERENCES experiments (id)
 ) ENGINE = INNODB;
@@ -28,8 +28,8 @@ CREATE TABLE IF NOT EXISTS benchmarks
 (
     experiment_id           INT,
     backup_pair_name        VARCHAR(255),
-    received_data_size_byte BIGINT,
-    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    received_data_size_byte BIGINT                              NOT NULL,
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (experiment_id, backup_pair_name),
     FOREIGN KEY (experiment_id, backup_pair_name) REFERENCES backup_pairs (experiment_id, name)
 ) ENGINE = INNODB;
