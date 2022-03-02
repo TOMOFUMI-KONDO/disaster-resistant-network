ed53837b1b9f9dba2f669cb534032ee87596fe8f
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-03-02 17:07:15 +0900

    feat: specify topology size by CLI flag

--- experiment/disaster_resistant_network_topo.py
+++ experiment/disaster_resistant_network_topo.py
@@ -1,42 +1,41 @@
+import random
+
 from mininet.link import TCLink
 from mininet.topo import Topo
 
 
 class DisasterResistantNetworkTopo(Topo):
+    BW_MIN_MBPS = 500
+    BW_MAX_MBPS = 1000
+
     def build(self, *args, **params):
         """
-        Topology is like below. (size = 2)
+        Topology is like below. (size = 3)
 
-        h1s --- s1 --(1G)-- s2 --- h2c
-                 |           |
-               (10M)       (100M)
-                 |           |
-        h2s --- s3 --(1G)-- s4 --- h1c
+        h1s --- s1 --- s2 --- s3 --- h2c
+                 |     |      |
+                s4 --- s5 --- s6
+                 |     |      |
+        h2s --- s7 --- s8 --- s9 --- h1c
         """
-        # TODO: create any given size topology
         # add switches
-        # size = params["size"]
-        # switches = []
-        # for i in range(size):
-        #     for j in range(size):
-        #         dpid = size * i + j + 1
-        #         switches.append(self.addSwitch(f"s{dpid}", dpid=str(dpid)))
-        #
+        size = params["size"]
+        switches = []
+        for i in range(size):
+            for j in range(size):
+                dpid = size * i + j + 1
+                switches.append(self.addSwitch(f"s{dpid}", dpid=str(dpid)))
+
         # # add links between switches
-        # for i in range(size):
-        #     for j in range(size):
-        #         current = switches[size * i + j]
-        #         if j != size - 1:
-        #             right = switches[size * i + j + 1]
-        #             self.addLink(current, right)
-        #         if i != size - 1:
-        #             bottom = switches[size * (i + 1) + j]
-        #             self.addLink(current, bottom)
-        switches = [self.addSwitch(f"s{i}", dpid=str(i)) for i in range(1, 5)]
-        self.addLink(switches[0], switches[1], cls=TCLink, bw=1000)  # bandwidth is specified with Mbps
-        self.addLink(switches[0], switches[2], cls=TCLink, bw=10)
-        self.addLink(switches[1], switches[3], cls=TCLink, bw=100)
-        self.addLink(switches[2], switches[3], cls=TCLink, bw=1000)
+        for i in range(size):
+            for j in range(size):
+                switch = switches[size * i + j]
+                if j != size - 1:
+                    right = switches[size * i + j + 1]
+                    self.addLink(switch, right, cls=TCLink, bw=self.__bandwidth())
+                if i != size - 1:
+                    bottom = switches[size * (i + 1) + j]
+                    self.addLink(switch, bottom, cls=TCLink, bw=self.__bandwidth())
 
         # NOTE: self.hosts() return hosts that are automatically ordered in alphabetically order
         # add hosts
@@ -48,10 +47,13 @@ class DisasterResistantNetworkTopo(Topo):
         ]
 
         # add links between host and switch
-        self.addLink(hosts[0], switches[3])
-        self.addLink(hosts[1], switches[0])
-        self.addLink(hosts[2], switches[1])
-        self.addLink(hosts[3], switches[2])
+        self.addLink(hosts[0], switches[size * size - 1])  # h1c
+        self.addLink(hosts[1], switches[0])  # h1s
+        self.addLink(hosts[2], switches[size - 1])  # h2c
+        self.addLink(hosts[3], switches[size * (size - 1)])  # h2s
+
+    def __bandwidth(self):
+        return random.randint(self.BW_MIN_MBPS, self.BW_MAX_MBPS)
 
 
 topos = {"disaster_resistant_network__topo": lambda: DisasterResistantNetworkTopo()}
