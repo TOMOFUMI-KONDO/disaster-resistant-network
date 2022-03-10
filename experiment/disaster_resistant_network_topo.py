592875e5d84dff1650d59ba8bf36244b6308e373
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2022-03-10 18:36:26 +0900

    feat: call controller API in creation of mininet topology to register network components dynamically

--- experiment/disaster_resistant_network_topo.py
+++ experiment/disaster_resistant_network_topo.py
@@ -1,12 +1,20 @@
+import json
 import random
 
+import requests
 from mininet.link import TCLink
 from mininet.topo import Topo
 
 
 class DisasterResistantNetworkTopo(Topo):
-    BW_MIN_MBPS = 500
-    BW_MAX_MBPS = 1000
+    __BW_MIN_MBPS = 500
+    __BW_MAX_MBPS = 1000
+
+    __URL = "http://localhost:8080"
+
+    def __init__(self, *args, **params):
+        super(DisasterResistantNetworkTopo, self).__init__(args, params)
+        self.__switch_port_counts = {}
 
     def build(self, *args, **params):
         """
@@ -24,54 +32,99 @@ class DisasterResistantNetworkTopo(Topo):
         for i in range(size):
             for j in range(size):
                 dpid = size * i + j + 1
-                switches.append(self.addSwitch(f"s{dpid}", dpid=str(dpid)))
+                name = f"s{dpid}"
+                switches.append(self.addSwitch(name, dpid=str(dpid)))
+                self.__switch_port_counts[name] = 0
 
         # add links between switches
-        # for i in range(size):
-        #     for j in range(size):
-        #         switch = switches[size * i + j]
-        #         if j != size - 1:
-        #             right = switches[size * i + j + 1]
-        #             self.addLink(switch, right, cls=TCLink, bw=self.__bandwidth())
-        #         if i != size - 1:
-        #             bottom = switches[size * (i + 1) + j]
-        #             self.addLink(switch, bottom, cls=TCLink, bw=self.__bandwidth())
-        # TODO: create links dynamically according to size
-        self.addLink(switches[0], switches[1], cls=TCLink, bw=779)
-        self.addLink(switches[0], switches[3], cls=TCLink, bw=605)
-        self.addLink(switches[1], switches[2], cls=TCLink, bw=861)
-        self.addLink(switches[1], switches[4], cls=TCLink, bw=748)
-        self.addLink(switches[2], switches[5], cls=TCLink, bw=813)
-        self.addLink(switches[3], switches[4], cls=TCLink, bw=550)
-        self.addLink(switches[3], switches[6], cls=TCLink, bw=662)
-        self.addLink(switches[4], switches[5], cls=TCLink, bw=610)
-        self.addLink(switches[4], switches[7], cls=TCLink, bw=681)
-        self.addLink(switches[5], switches[8], cls=TCLink, bw=524)
-        self.addLink(switches[6], switches[7], cls=TCLink, bw=786)
-        self.addLink(switches[7], switches[8], cls=TCLink, bw=753)
+        for i in range(size):
+            for j in range(size):
+                switch = switches[size * i + j]
+                if j != size - 1:
+                    right = switches[size * i + j + 1]
+                    self.__switch_port_counts[switch] += 1
+                    self.__switch_port_counts[right] += 1
 
+                    self.__add_link(switch, self.__switch_port_counts[switch], right, self.__switch_port_counts[right])
+                    self.addLink(switch, right, cls=TCLink, bw=self.__rand_bandwidth())
+
+                if i != size - 1:
+                    bottom = switches[size * (i + 1) + j]
+                    self.__switch_port_counts[switch] += 1
+                    self.__switch_port_counts[bottom] += 1
+
+                    self.__add_link(switch, self.__switch_port_counts[switch],
+                                    bottom, self.__switch_port_counts[bottom])
+                    self.addLink(switch, bottom, cls=TCLink, bw=self.__rand_bandwidth())
+
+        # NOTE: assume size >= 3
         # NOTE: self.hosts() return hosts that are automatically ordered in alphabetically order
-        # add hosts
-        hosts = [
-            self.addHost(f"h1c", ip=f"10.0.0.1", mac=f"00:00:00:00:00:01"),
-            self.addHost(f"h1s", ip=f"10.0.0.2", mac=f"00:00:00:00:00:02"),
-            self.addHost(f"h2c", ip=f"10.0.0.3", mac=f"00:00:00:00:00:03"),
-            self.addHost(f"h2s", ip=f"10.0.0.4", mac=f"00:00:00:00:00:04"),
-            self.addHost(f"h3c", ip=f"10.0.0.5", mac=f"00:00:00:00:00:05"),
-            self.addHost(f"h3s", ip=f"10.0.0.6", mac=f"00:00:00:00:00:06"),
-        ]
+        # add 3 host pairs
+        switch_for_h1c = switches[size - 1]
+        self.__switch_port_counts[switch_for_h1c] += 1
+        switch_for_h1s = switches[size]
+        self.__switch_port_counts[switch_for_h1s] += 1
+        self.__add_host_pair("h1c", self.__switch_port_counts[switch_for_h1c],
+                             "10.0.0.1", "00:00:00:00:00:01", switch_for_h1c,
+                             "h1s", self.__switch_port_counts[switch_for_h1s],
+                             "10.0.0.2", "00:00:00:00:00:02", switch_for_h1s)
 
-        # NOTE: assume size is 3
-        # add links between host and switch
-        self.addLink(hosts[0], switches[2])
-        self.addLink(hosts[1], switches[3])
-        self.addLink(hosts[2], switches[5])
-        self.addLink(hosts[3], switches[6])
-        self.addLink(hosts[4], switches[8])
-        self.addLink(hosts[5], switches[0])
+        switch_for_h2c = switches[size * (size // 2 + 1) - 1]
+        self.__switch_port_counts[switch_for_h2c] += 1
+        switch_for_h2s = switches[size * (size - 1)]
+        self.__switch_port_counts[switch_for_h2s] += 1
+        self.__add_host_pair("h2c", self.__switch_port_counts[switch_for_h2c],
+                             "10.0.0.3", "00:00:00:00:00:03", switch_for_h2c,
+                             "h2s", self.__switch_port_counts[switch_for_h2s],
+                             "10.0.0.4", "00:00:00:00:00:04", switch_for_h2s)
 
-    def __bandwidth(self):
-        return random.randint(self.BW_MIN_MBPS, self.BW_MAX_MBPS)
+        switch_for_h3c = switches[size * size - 1]
+        self.__switch_port_counts[switch_for_h3c] += 1
+        switch_for_h3s = switches[0]
+        self.__switch_port_counts[switch_for_h3s] += 1
+        self.__add_host_pair("h3c", self.__switch_port_counts[switch_for_h3c],
+                             "10.0.0.5", "00:00:00:00:00:05", switch_for_h3c,
+                             "h3s", self.__switch_port_counts[switch_for_h3s],
+                             "10.0.0.6", "00:00:00:00:00:06", switch_for_h3s)
+
+    def __add_link(self, switch1: str, switch1_port: int, switch2: str, switch2_port: int, bw: int = None, cls=TCLink):
+        if bw is None:
+            bw = self.__rand_bandwidth()
+
+        # add link to mininet
+        self.addLink(switch1, switch2, cls=cls, bw=bw)
+
+        # register link to controller
+        requests.post(self.__URL + "/link", data=json.dumps({
+            "switch1": {"name": switch1, "port": switch1_port},
+            "switch2": {"name": switch2, "port": switch2_port},
+            "bandwidth_mbps": bw,
+        }))
+
+    def __add_host_pair(self, client_name: str, client_port: int, client_ip: str, client_mac: str, client_neighbor: str,
+                        server_name: str, server_port: int, server_ip: str, server_mac: str, server_neighbor: str):
+        self.addHost(client_name, ip=client_ip, mac=client_mac)
+        self.addHost(server_name, ip=server_ip, mac=server_mac)
+        self.addLink(client_name, client_neighbor)
+        self.addLink(server_name, server_neighbor)
+
+        requests.post(self.__URL + "/host-pair", data=json.dumps({
+            "client": {
+                "name": client_name,
+                "port": client_port,
+                "ip_address": client_ip,
+                "neighbor": client_neighbor
+            },
+            "server": {
+                "name": server_name,
+                "port": server_port,
+                "ip_address": server_ip,
+                "neighbor": server_neighbor
+            }
+        }))
+
+    def __rand_bandwidth(self):
+        return random.randint(self.__BW_MIN_MBPS, self.__BW_MAX_MBPS)
 
 
 topos = {"disaster_resistant_network__topo": lambda: DisasterResistantNetworkTopo()}
